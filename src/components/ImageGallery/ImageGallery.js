e7807a9e35cc4c14a5e85b2a13231be5e2e67a10
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2023-03-01 22:07:01 +0100

    fix depences

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -4,13 +4,13 @@ import css from './ImageGallery.module.css';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import React, { useEffect } from 'react';
 export const ImageGallery = ({ images, onClick }) => {
-  const scrollToBottom = () => {
-    window.scrollBy({
-      top: 600,
-      behavior: 'smooth',
-    });
-  };
   useEffect(() => {
+    const scrollToBottom = () => {
+      window.scrollBy({
+        top: 600,
+        behavior: 'smooth',
+      });
+    };
     scrollToBottom();
   }, [images]);
 
